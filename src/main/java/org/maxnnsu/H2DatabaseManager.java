ac07c806a721aaebf41badc9547fbcbec1113368
Tsvetkov Maksim <tsvetkovm@ae.com>
2023-07-14 10:19:33 +0200

    moved to the Data type

--- src/main/java/org/maxnnsu/H2DatabaseManager.java
+++ src/main/java/org/maxnnsu/H2DatabaseManager.java
@@ -17,8 +17,6 @@ public class H2DatabaseManager {
     //private static final String DB_URL = "jdbc:h2:./db/dosardata.db";
     private static final String DB_INIT_URL = "jdbc:h2:./src/main/resources/db/dosardata.db";
     private static final String DB_URL = "jdbc:h2:./dosardata.db";
-    private static final String DB_USERNAME = "логин";
-    private static final String DB_PASSWORD = "пароль";
 
     public static void createDosarTables() {
         try (Connection connection = DriverManager.getConnection(DB_INIT_URL);
@@ -26,10 +24,10 @@ public class H2DatabaseManager {
                      "CREATE TABLE IF NOT EXISTS dosar_data (" +
                              "id INT AUTO_INCREMENT PRIMARY KEY," +
                              "request_document_name VARCHAR(255) UNIQUE," +
-                             "request_date VARCHAR(255)," +
-                             "original_review_date VARCHAR(255)," +
+                             "request_date DATE," +
+                             "original_review_date DATE," +
                              "conclusion_document_name VARCHAR(255)," +
-                             "actual_review_date VARCHAR(255)" +
+                             "actual_review_date DATE" +
                              ")"
              );
              PreparedStatement statement2 = connection.prepareStatement(
@@ -55,31 +53,12 @@ public class H2DatabaseManager {
                              "conclusion_document_name, actual_review_date) " +
                              "VALUES (?, ?, ?, ?, ?)"
              )) {
-            switch (dosarDataModel.getRecordSize()) {
-                case 2 -> {
-                    statement.setString(1, dosarDataModel.getRequestDocumentName());
-                    statement.setString(2, dosarDataModel.getRequestDate().toString());
-                    statement.setString(3, null);
-                    statement.setString(4, null);
-                    statement.setString(5, null);
-                }
-                case 3 -> {
-                    statement.setString(1, dosarDataModel.getRequestDocumentName());
-                    statement.setString(2, dosarDataModel.getRequestDate().toString());
-                    statement.setString(3, dosarDataModel.getOriginalReviewDate().toString());
-                    statement.setString(4, null);
-                    statement.setString(5, null);
-                }
-                case 5 -> {
-                    statement.setString(1, dosarDataModel.getRequestDocumentName());
-                    statement.setString(2, dosarDataModel.getRequestDate().toString());
-                    statement.setString(3, dosarDataModel.getOriginalReviewDate().toString());
-                    statement.setString(4, dosarDataModel.getConclusionDocumentName());
-                    statement.setString(5, dosarDataModel.getActualReviewDate().toString());
-                }
-                default -> {
-                }
-            }
+
+            statement.setString(1, dosarDataModel.getRequestDocumentName());
+            statement.setDate(2, dosarDataModel.getRequestDate());
+            statement.setDate(3, dosarDataModel.getOriginalReviewDate());
+            statement.setString(4, dosarDataModel.getConclusionDocumentName());
+            statement.setDate(5, dosarDataModel.getActualReviewDate());
 
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -168,8 +147,8 @@ public class H2DatabaseManager {
             if (resultSet.next()) {
                 DosarDataModel existingDosarDataModel = new DosarDataModel(resultSet);
                 if (!existingDosarDataModel.equals(dosarDataModel)) {
-                    updateStatement.setDate(1, Objects.nonNull(dosarDataModel.getActualReviewDate()) ? new java.sql.Date(dosarDataModel.getActualReviewDate().getTime()) : null);
-                    updateStatement.setDate(2, Objects.nonNull(dosarDataModel.getOriginalReviewDate()) ? new java.sql.Date(dosarDataModel.getOriginalReviewDate().getTime()) : null);
+                    updateStatement.setDate(1, dosarDataModel.getActualReviewDate());
+                    updateStatement.setDate(2, dosarDataModel.getOriginalReviewDate());
                     updateStatement.setString(3, dosarDataModel.getConclusionDocumentName());
                     updateStatement.setString(4, dosarDataModel.getRequestDocumentName());
                     updateStatement.executeUpdate();
